789219148e5890e638800dc77c928864bbb36608
Hasheeb Kazi <hasheeb.k@gmail.com>
2019-02-03 10:52:09 -0500

    backend almost done

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const inputs = [];
-
+x=1;
 // routes
 app.post('/msg', (req, res, next) => {
     // const twiml = new MessagingResponse();
@@ -33,29 +33,36 @@ app.post('/msg', (req, res, next) => {
     var msgFrom = req.body.From;
     var msgBody = req.body.Body;
 
+ 
+
     if ( msgBody === '1') {
         // inputs.push({ "player_1": msgFrom});
         inputs.push({ "player": msgFrom, "order": "1"});
-        fs.writeFileSync('./data/player_1.json', JSON.stringify({ "player": msgFrom, "order": "1"}));
+        fs.writeFileSync('./data/player_1.json', JSON.stringify({ "player_1": msgFrom, "order": "1", "char": "nan"}));
         // fs.writeFileSync('./public/player_1.json', JSON.stringify({ "player": msgFrom, "order": "1"}));
-
+        player1 = msgFrom;
         // console.log(inputs);
     } else if ( msgBody === '2') {
         inputs.push({ "player": msgFrom, "order": "2"});
-        fs.writeFileSync('./data/player_2.json', JSON.stringify({ "player_2": msgFrom, "order": "2"}));
-        // fs.writeFileSync('./public/player_2.json', JSON.stringify({ "player_2": msgFrom, "order": "2"}));        
-        // inputs.push({ "player_2": msgFrom});
-        // console.log(inputs);
+        fs.writeFileSync('./data/player_2.json', JSON.stringify({ "player_2": msgFrom, "order": "2", "char": "nan"}));
+        player2 = msgFrom;
+
     } else if ( msgBody === 'a' || msgBody === 'b' || msgBody === 'c' || msgBody === 'A' || msgBody === 'B' || msgBody === 'C') {
         const num = utils.randNum();
         inputs.push({ "number": msgFrom, "input": msgBody.toLowerCase(), "auth": num});
 
         fs.writeFileSync('./data/moves.json', JSON.stringify({ "number": msgFrom, "input": msgBody.toLowerCase(), "auth": num}));
         // fs.writeFileSync('./public/moves.json', JSON.stringify({ "player_2": msgFrom, "order": "2"}));    
+    } else if ( msgBody.toLowerCase() === 'olavasaur' || msgBody.toLowerCase() === 'duckychu' || msgBody.toLowerCase() === 'penguinator' || msgBody.toLowerCase() === 'kaffine') {
+        // inputs.push({ "player_1": msgFrom});
+        fs.writeFileSync('./data/c' + x + '.json', JSON.stringify({ "player": msgFrom, "char": msgBody, "order": x}));
+        if (x === 1) {
+            x = 2;
+        } else {
+            x = 1;
+        }
     } else {
-        // console.log('Error invalid input.');
-        // console.log(msgBody);
-        res.redirect('/');
+        res.send('<html><body><h1>Error</h1></body></html>');
     }
 });
 
@@ -63,9 +70,13 @@ app.get('/admin/del', (req, res) => {
     // utils.delData("./data/player_1.json");
     // utils.delData("./data/player_2.json");
     // utils.delData("./data/moves.json");
-    fs.writeFileSync('./data/player_1.json', JSON.stringify({ "player_1": "nan", "order": "nan"}));
-    fs.writeFileSync('./data/player_2.json', JSON.stringify({ "player_2": "nan", "order": "nan"}));
+    fs.writeFileSync('./data/player_1.json', JSON.stringify({ "player_1": "nan", "order": "nan", "char": "nan"}));
+    fs.writeFileSync('./data/player_2.json', JSON.stringify({ "player_2": "nan", "order": "nan", "char": "nan"}));
     fs.writeFileSync('./data/moves.json', JSON.stringify({ "number": "nan", "input": "nan", "auth": "nan"}));
+    fs.writeFileSync('./data/c1.json', JSON.stringify({ "player": "nan", "char": "nan", "order": "nan"}));
+    fs.writeFileSync('./data/c2.json', JSON.stringify({ "player": "nan", "char": "nan", "order": "nan"}));
+
+
 
     res.send('<html><body><h1>OK</h1></body></html>');
 });
@@ -94,6 +105,26 @@ app.get('/data/p2', (req, res, next) => {
 
 app.get('/data/m', (req, res, next) => {
     var json = JSON.parse(fs.readFileSync('./data/moves.json', 'utf8', (data, error) => {
+        if (error) {
+            console.log('error');
+        }
+    }));
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(json);
+});
+
+app.get('/data/c1', (req, res, next) => {
+    var json = JSON.parse(fs.readFileSync('./data/c1.json', 'utf8', (data, error) => {
+        if (error) {
+            console.log('error');
+        }
+    }));
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(json);
+});
+
+app.get('/data/c2', (req, res, next) => {
+    var json = JSON.parse(fs.readFileSync('./data/c2.json', 'utf8', (data, error) => {
         if (error) {
             console.log('error');
         }
